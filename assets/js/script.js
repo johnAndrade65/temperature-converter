c8be1515ed0d959bf934bcb768feee9a1951c1cd
john65 <joaovini2667@gmail.com>
2022-10-20 21:24:58 -0300

    version 1.2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,10 @@ console.log(celsius, fahrenheit);
 
 
 function celToFar(){
-    let output = ( parseFloat(celsius.value) * 9/5) + 32;
-    fahrenheit.value = parseFloat(output.toFixed(2));
+    let output = ( parseInt(celsius.value) * 9/5) + 32;
+    fahrenheit.value = parseInt(output.toFixed(2));
 }
 function farToCel(){
-    let output = ( parseFloat(fahrenheit.value) - 32) * 5/9;
-    celsius.value = parseFloat(output.toFixed(2));
+    let output = ( parseInt(fahrenheit.value) - 32) * 5/9;
+    celsius.value = parseInt(output.toFixed(2));
 }
