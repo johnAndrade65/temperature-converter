b7f0efd22579f7c71ab20ba864ce33c2f696b640
john65 <joaovini2667@gmail.com>
2022-10-22 10:09:44 -0300

    version 1.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,18 @@ function farToCel(){
     let output = ( parseInt(fahrenheit.value) - 32) * 5/9;
     celsius.value = parseInt(output.toFixed(2));
 }
+function alertMsg(){
+    if(celsius.value == 0){
+        alert("Type something 🤓")
+    }else if(celsius.value <= -10){
+        alert("I froze 🥶")
+    }else if(celsius.value <= 7){
+        alert("It's cold! 🥶")
+    }else if(celsius.value <= 17){
+        alert("Nice weather 🥰")
+    }else if(celsius.value <= 30 && celsius.value <= 35){
+        alert("It's hot 🥵")
+    }else{
+        alert("I melted ☠")
+    }
+}
